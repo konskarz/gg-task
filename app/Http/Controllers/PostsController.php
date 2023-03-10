70cd8b5db3f8ff75c812fff7c01c049a893b1769
Konstantin Karzanov <k.karzanov@gmail.com>
2023-03-10 20:27:14 +0100

    add swagger api docs

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,6 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
+/**
+ * @OA\Info(
+ *     description="Create, Read, Update, Delete Posts",
+ *     version="1.0.0",
+ *     title="Posts API"
+ * )
+ */
+
 class PostsController extends Controller
 {
     private $http;
@@ -17,40 +25,80 @@ class PostsController extends Controller
     }
 
     /**
-     * Display a listing of the resource.
-     */
+         * @OA\Get(
+         *     path="/api/posts",
+         *     summary="Get all posts",
+         *     description="Display a listing of the resource.",
+         *     @OA\Response(
+         *         response=200,
+         *         description="Everything OK"
+         *     ),
+         * )
+    */
     public function index()
     {
         return $this->http->get('posts')->json();
     }
 
     /**
-     * Store a newly created resource in storage.
-     */
+         * @OA\Post(
+         *     path="/api/posts",
+         *     summary="Create a new post",
+         *     description="Store a newly created resource in storage.",
+         *     @OA\Response(
+         *         response=200,
+         *         description="Everything OK"
+         *     ),
+         * )
+    */
     public function store(Request $request)
     {
         return $this->http->post('posts', $request->json())->json();
     }
 
     /**
-     * Display the specified resource.
-     */
+         * @OA\Get(
+         *     path="/api/posts/:id",
+         *     summary="Get specific post",
+         *     description="Display the specified resource.",
+         *     @OA\Response(
+         *         response=200,
+         *         description="Everything OK"
+         *     ),
+         * )
+    */
     public function show(string $id)
     {
         return $this->http->get("posts/{$id}")->json();
     }
 
     /**
-     * Update the specified resource in storage.
-     */
+         * @OA\Put(
+         *     path="/api/posts/:id",
+         *     summary="Update specific post",
+         *     description="Update the specified resource in storage.",
+         *     @OA\Response(
+         *         response=200,
+         *         description="Everything OK"
+         *     ),
+         * )
+    */
     public function update(Request $request, string $id)
     {
         return $this->http->put("posts/{$id}", $request->json())->json();
     }
 
     /**
-     * Remove the specified resource from storage.
-     */
+         * @OA\Delete(
+         *     path="/api/posts/:id",
+         *     summary="Delete specific post",
+         *     description="Remove the specified resource from storage.",
+         *     @OA\Response(
+         *         response=200,
+         *         description="Everything OK"
+         *     ),
+         * )
+    */
     public function destroy(string $id)
     {
         return $this->http->delete("posts/{$id}")->json();
